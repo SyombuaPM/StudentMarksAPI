e1d34e223306c687bde2fa99b32d45ca6e391efd
Priscilla Mwendwa <priscilla@Home-book.local>
2024-03-03 21:33:44 +0300

    Learning

---
+++ Services/StudentService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentAPI;
+
+public class StudentService
+{
+    private readonly StudentDBContext _context;
+
+    public StudentService(StudentDBContext context)
+    {
+        _context = context;
+    }
+
+    public List<Student> GetStudents()
+    {
+        return _context.Students.ToList();
+    }
+
+      public async Task<List<Student>> GetStudentsAsync()
+    {
+        var list = _context.Students.ToListAsync();
+        Task.Delay(1000);
+        return  await list;
+    }
+
+    public Student GetStudent(int id)
+    {
+        return _context.Students.FirstOrDefault(s => s.Id == id);
+    }
+
+    public void AddStudent(Student student)
+    {
+        _context.Students.Add(student);
+        _context.SaveChanges();
+    }
+
+    public void UpdateStudent(Student student)
+    {
+        _context.Entry(student).State = EntityState.Modified;
+        _context.SaveChanges();
+    }
+
+    public void DeleteStudent(int id)
+    {
+        var student = _context.Students.Find(id);
+        _context.Students.Remove(student);
+        _context.SaveChanges();
+    }
+
+}
